1d708cbe2ff8462ef154c05cde96506f79f511be
glynn29 <glynn_29@hotmail.com>
2020-08-22 12:49:46 -0500

    Initial client side pages

---
+++ src/containers/Calendar/Calendar.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import 'react-calendar/dist/Calendar.css'
+
+import Calendar from 'react-calendar';
+import * as classes from './Calendar.module.css';
+
+const CalendarBox = () => {
+    const [date, setDate] = useState(new Date());
+
+    const links = {
+        link1: <a href="www.google.com">Google</a>
+    };
+
+    return (
+        <div>
+
+            <p>calendar page</p>
+
+
+                <Calendar
+                    className={classes.Box}
+                    onChange={e => setDate(e)}
+                    value={date}
+                    defaultValue={new Date()}
+                    tileClassName={classes.Tile}
+                    calendarType={"US"}
+                    showNeighboringMonth={false}
+                    nextLabel={"Next Month >"}
+                    next2Label={"Next Year >"}
+                    prevLabel={"< Previous Month"}
+                    prev2Label={"< Previous Year"}
+                    //onClickDay={(date) => alert(date)}
+                    //tileContent={links.link1}
+                />
+            <p>{date.toDateString()}</p>
+        </div>);
+};
+
+export default CalendarBox;
---
+++ src/containers/Client/Account/Account.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Account = () => {
+    return (<div>
+        <p>Account page</p>
+    </div>);
+};
+
+export default Account;
