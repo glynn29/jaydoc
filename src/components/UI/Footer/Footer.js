0a5076d27994caedaaf711a76813dee91aed825b
glynn29 <glynn_29@hotmail.com>
2020-08-22 12:47:55 -0500

    Footer component and styles file for Material UI

---
+++ src/components/UI/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import * as classes from './Footer.module.css'
+
+function Copyright() {
+    return (
+        <Typography className={classes.Footer} variant="body2" color="textSecondary" align="center">
+            Software
+            {' © '}
+
+            {new Date().getFullYear() + ' '}
+            <Link color="inherit" href="">
+                Glynn Leininger
+            </Link>
+            {'.'}
+        </Typography>
+    );
+}
+
+export default function Footer() {
+    return <Copyright/>;
+}
