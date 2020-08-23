26b8910e74b5a702df62eef648581d98f7bfc05f
glynn29 <glynn_29@hotmail.com>
2020-08-23 17:55:30 -0500

    event tables for data and form for filling out data

--- src/components/UI/Button/EditEvent/EditEvent.js
+++ src/components/UI/Button/EditEvent/EditEvent.js
@@ -1,41 +1,17 @@
 import React, {useState} from "react";
-import TransitionModal from "../../Modal/Modal";
 
-import Typography from "@material-ui/core/Typography";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import FormControl from "@material-ui/core/FormControl";
 import formStyles from "../../Styles/formStyle";
 import TextField from "@material-ui/core/TextField";
-import * as classes from './EditEvent.module.css'
 import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-
-const list = [
-    {name:"M1"},
-    {name:"M2"},
-    {name:"M3"},
-    {name:"M4"},
-];
 
 const EditEvent= props => {
     const formClasses = formStyles();
-    const [open, setOpen] = React.useState(false);
-    const [name, setName] = useState("");
-    const [sponsor, setSponsor] = useState("");
-    const [details, setDetails] = useState("");
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
+    const [name, setName] = useState(props.formData.name);
+    const [sponsor, setSponsor] = useState(props.formData.sponsor);
+    const [details, setDetails] = useState(props.formData.details);
 
     const submitFormHandler = () =>{
             alert(name + " " + sponsor + " " + details);
@@ -44,6 +20,7 @@ const EditEvent= props => {
     const form = (
         <div>
         <CssBaseline />
+        <h1>{"Id is: " + props.formData.id}</h1>
         <form className={formClasses.root} noValidate autoComplete="off" onSubmit={submitFormHandler}>
             <Grid container spacing={2} direction={"column"} alignItems={"stretch"}>
                 <Grid item>
@@ -101,14 +78,7 @@ const EditEvent= props => {
         </div>
     );
 
-    return(
-      <div>
-          <Button variant="contained" color="secondary" type="button" onClick={handleOpen}>
-              Edit
-          </Button>
-          <TransitionModal open={open} handleOpen={handleOpen} handleClose={handleClose} form={form} title={"Edit Event"}/>
-      </div>
-    );
+    return form;
 };
 
 export default EditEvent;
--- src/containers/Admin/EventList/EventList.js
+++ src/containers/Admin/EventList/EventList.js
@@ -1,9 +1,84 @@
-import React from "react";
+import React, {useState} from "react";
+import EnhancedTable from "../../../components/UI/Table/Table";
+import EditEventForm from "../../../components/UI/Button/EditEvent/EditEvent";
+import TransitionModal from "../../../components/UI/Modal/Modal";
+
+function createData(name, sponsor, id) {
+    return { name, sponsor, id };
+}
+
+const headCells = [
+    { id: 'name', label: 'Event Name' },
+    { id: 'sponsor', label: 'Sponsor' },
+    { id: 'id',  label: 'ID' },
+];
+
+const rowLabels = [
+    { id: 'name' },
+    { id: 'sponsor' },
+    { id: 'id',   }
+];
+
+let rows = [
+    createData('Womens health nightttt', "foo1", 6, ),
+    createData('Womens health night2', "foo2", 62, ),
+    createData('Womens health night3', "foo3", 36, ),
+    createData('Womens health night4', "foo4", 632, ),
+];
+
+function deleteEvent(eventId) {
+    alert("removed id : " + eventId)
+}
+
+function addEvent() {
+    alert("Event added")
+}
+
+function detailsEvent(eventId) {
+    alert("details Event id:" + eventId)
+}
+
+const formInfo = {name: "Ladies night", sponsor: "foo", id: 12 };
 
 const EventList = () => {
-    return (<div>
-        <p>Event page</p>
-    </div>);
+    const [open, setOpen] = useState(false);
+    const [formData, setFormData] = useState(formInfo);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const toggleEdit = (eventId) => {
+        setFormData({...formData, id: eventId});
+        setOpen(!open);
+
+    };
+
+    return (
+        <div>
+            <p>Event page</p>
+            <EnhancedTable
+                data={rows}
+                headCells={headCells}
+                rowLables={rowLabels}
+                delete={deleteEvent}
+                add={addEvent}
+                edit={toggleEdit}
+                details={detailsEvent}
+            />
+            <TransitionModal
+                open={open}
+                handleOpen={handleOpen}
+                handleClose={handleClose}
+                form={<EditEventForm formData={formData}/>}
+                title={"Edit Event"}
+            />
+        </div>
+    );
 };
 
 export default EventList;
