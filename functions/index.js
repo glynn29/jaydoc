5dc40a3f81a866b18935bf166086cb79d11e2a91
glynn29 <glynn_29@hotmail.com>
2020-09-01 17:23:11 -0500

    added firebase and firebase helper function

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context)=> {
+    return admin.auth().getUserByEmail(data.email).then((user) => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin:true
+        });
+    }).then(() => {
+        return {
+            message: 'success user has been made admin'
+        }
+    }).catch(error => {return error})
+});
