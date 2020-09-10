0732288d01ecce993f7908061b58f9b56aace58a
glynn29 <glynn_29@hotmail.com>
2020-09-10 17:30:01 -0500

    added confirmation page route

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import Register from "./containers/Auth/Register/Register";
 import ContactUs from "./containers/Client/ContactUs/ContactUs";
 import Comment from "./containers/Client/Comments/Comment";
 import Calendar from "./containers/Calendar/Calendar";
+import Confirmation from "./containers/Calendar/Confirmation/Confirmation";
 import Account from "./containers/Client/Account/Account";
 import Home from "./containers/Client/Home/Home";
 import Logout from "./containers/Auth/Logout/Logout";
 import Dashboard from "./containers/Admin/Dashboard/Dashboard";
 import Email from "./containers/Admin/Email/Email";
 import EventList from "./containers/Admin/EventList/EventList";
+import ScheduledEventList from "./containers/Admin/ScheduledEventList/ScheduledEventList";
 import Report from "./containers/Admin/Report/Reports";
 import Users from "./containers/Admin/Users/Users";
 import Layout from "./containers/hoc/Layout/Layout";
 import {AuthContext} from "./containers/Auth/Auth";
 import * as actions from './store/actions/index';
+//import firebase from "./firebase";
+//import withErrorHandler from "./containers/hoc/withErrorHandler/withErrorHandler";
 
 const App = (props) =>{
     const {currentUser, isAdmin} = useContext(AuthContext);
@@ -47,6 +51,7 @@ const App = (props) =>{
                 <Route path="/comments" component={Comment}/>
                 <Route path="/logout" component={Logout}/>
                 <Route path="/login" component={Login}/>
+                <Route path="/confirm" component={Confirmation}/>
                 <Route path="/" exact component={Home}/>
             </Switch>
         );
@@ -59,6 +64,7 @@ const App = (props) =>{
                 <Route path="/report" component={Report}/>
                 <Route path="/eventList" component={EventList}/>
                 <Route path="/volunteerList" component={Users}/>
+                <Route path="/scheduledEventList" component={ScheduledEventList}/>
                 <Route path="/logout" component={Logout}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/" exact component={Dashboard}/>
