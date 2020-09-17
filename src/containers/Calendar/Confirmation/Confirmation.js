87669761f078acc8789c08dd5086cb214c1cdab1
glynn29 <glynn_29@hotmail.com>
2020-09-16 21:28:40 -0500

    Confirmation page to show user they have successfully signed up

--- src/containers/Calendar/Confirmation/Confirmation.js
+++ src/containers/Calendar/Confirmation/Confirmation.js
@@ -1,9 +1,29 @@
 import React from "react";
+import {connect} from "react-redux";
+import {Redirect} from "react-router-dom";
+
+import Container from "@material-ui/core/Container";
 
 const Confirmation = props => {
+    if(!props.formData){
+        return <Redirect to={"/"}/>
+    }
+
+    const {formData} = props;
+    console.log(props.formData);
+
     return(
-        <div></div>
+        <Container component="main" maxWidth="md" style={{textAlign: 'center'}}>
+                <p>Sign Up Successful!</p>
+                <p>{formData.eventName}</p>
+                <p>You signed up as {formData.position} from {formData.startTime} to {formData.endTime} on {formData.date}?</p>
+        </Container>
     )
 };
 
-export default Confirmation;
+const mapStateToProps = state => {
+    return{
+        formData: state.signUp.data
+    };
+};
+export default connect(mapStateToProps)(Confirmation);
