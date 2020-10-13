d5b11129a60f741ccabfe1a75aa1dfc60e249fea
glynn29 <glynn_29@hotmail.com>
2020-10-12 21:19:42 -0500

    fixed form button color and style

--- src/containers/Calendar/Forms/AreYouSure/AreYouSure.js
+++ src/containers/Calendar/Forms/AreYouSure/AreYouSure.js
@@ -1,25 +1,40 @@
-import React, {useState} from "react";
+import React from "react";
 
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
-import useStyles from "../../../../components/UI/Styles/formStyle";
 import Grid from "@material-ui/core/Grid";
+
+import useStyles from "../../../../components/UI/Styles/formStyle";
 
 const AreYouSure = props => {
     const classes = useStyles();
-    const [formData, setFormData] = useState(props.formData);
+    const {formData} = props;
 
     return(
-        <Container component="main" maxWidth="md" style={{textAlign: 'center'}}>
+        <Container component="main" maxWidth="sm" style={{textAlign: 'center'}}>
             <Grid container spacing={1} direction={"column"} alignItems={"stretch"} >
                 <p>{formData.eventName}</p>
                 <p>{formData.name}, are you Sure you want to signup for {formData.position} from {formData.startTime} to {formData.endTime} on {formData.date}?</p>
                 <p>Once you signup you will need Admin approval to cancel</p>
-                <Grid item>
-                    <Button variant="contained" className={classes.deleteButton} onClick={props.cancel}>Cancel</Button>
+                <Grid item xs={6}>
+                    <Button
+                        variant="outlined"
+                        className={classes.cancelButton}
+                        onClick={props.cancel}
+                        fullWidth
+                    >
+                        Cancel
+                    </Button>
                 </Grid>
-                <Grid item>
-                    <Button variant="contained"  className={classes.detailsButton} onClick={() => props.submit({index: formData.index, position: formData.position, date: formData.date})}>Confirm</Button>
+                <Grid item xs={6}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => props.submit({index: formData.index, position: formData.position, date: formData.date})}
+                        fullWidth
+                    >
+                        Confirm
+                    </Button>
                 </Grid>
             </Grid>
         </Container>
