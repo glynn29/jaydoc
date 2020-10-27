3601e15fb99528b6c7f4280512c20b815688c4e5
glynn29 <glynn_29@hotmail.com>
2020-10-26 23:01:03 -0500

    add drop down and different pages

--- src/containers/Admin/Dashboard/Dashboard.js
+++ src/containers/Admin/Dashboard/Dashboard.js
@@ -1,11 +1,102 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+
+import {firestore} from "../../../firebase";
+import {AddAdmin} from "../../../CloudFunctions/addAdmin";
+import AdminContact from "./Forms/AdminContact/AdminContact";
+import AdminComments from "./Forms/AdminComments/AdminComments";
+import Cancellation from "./Forms/Cancellation/Cancellation";
+import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import formStyles from "../../../components/UI/Styles/formStyle";
 
 const Dashboard = () => {
+    const classes = formStyles();
+    const [contact, setContact] = useState([]);
+    const [comment, setComment] = useState([]);
+    const [page, setPage] = useState('contact');
+
+    const list = [
+        'contact',
+        'comment',
+        'cancel',
+    ];
+
+    useEffect(() => {
+        getContact().catch();
+        getComment().catch();
+    },[]);
+
+    async function getContact (){
+        let contactList = [];
+        const ref = await firestore.collection('contact').get();
+        ref.forEach((res) => {
+            contactList.push(res.data());
+        });
+        setContact(contactList);
+    }
+
+    async function getComment (){
+        let commentList = [];
+        const ref = await firestore.collection('comments').get();
+        ref.forEach((res) => {
+            commentList.push(res.data());
+        });
+        setComment(commentList);
+    }
+
+    const getElement = () => {
+        switch(page){
+            case 'contact':
+                return <AdminContact contact={contact}/>;
+            case 'comment':
+                return <AdminComments comments={comment}/>;
+            default:
+                return <Cancellation/>;
+        }
+    };
+
     return (
         <Container component="main" maxWidth="md" style={{textAlign: 'center'}}>
-            <Typography variant="h3">Admin Dashboard Page</Typography>
+            <Grid container spacing={2} direction={"column"} alignItems={"stretch"}>
+                <Grid item>
+                    <Typography variant="h3">Admin Dashboard Page</Typography>
+                </Grid>
+                <Grid item>
+                    <FormControl variant="outlined" className={classes.formControl} >
+                        <InputLabel htmlFor="outlined-age-native-simple">Page</InputLabel>
+                        <Select
+                            value={page}
+                            onChange={event => setPage(event.target.value) }
+                            label="Page"
+                            inputProps={{
+                                name: 'page',
+                                id: 'outlined-age-native-simple',
+                            }}
+                        >
+                            {list.map( listItem => {
+                                return (
+                                    <MenuItem key={listItem} value={listItem}>{listItem}</MenuItem>
+                                );
+                            })}
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <br/>
+                {
+                    getElement()
+                }
+            </Grid>
+            {/*<AddAdmin/>*/}
+            {/*<br/>*/}
+            {/*<AdminContact contact={contact}/>*/}
+            {/*<AdminComments comments={comment}/>*/}
         </Container>
     );
 };
