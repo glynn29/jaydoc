580ed4a2c51a609e9bf941d23dda893b5f3f9aec
glynn29 <glynn_29@hotmail.com>
2020-11-02 23:51:17 -0600

    added delete functionality

--- src/containers/Admin/Dashboard/Forms/AdminComments/AdminComments.js
+++ src/containers/Admin/Dashboard/Forms/AdminComments/AdminComments.js
@@ -1,20 +1,50 @@
 import React, {useEffect, useState} from "react";
+
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+
 import EnhancedTable from "../../../../../components/UI/Table/Table";
+import DeleteComment from "../DeleteComment/DeleteComment";
+import TransitionModal from "../../../../../components/UI/Modal/Modal";
+import {firestore} from "../../../../../firebase";
+
+const headCells = [
+    {id: 'subject', label: 'Subject'},
+    {id: 'message', label: 'Message'}
+];
 
 const AdminContact = (props) => {
     const [comments, setComments] = useState([]);
+    const [formData, setFormData] = useState({});
+    const [deleteOpen, setDeleteOpen] = useState(false);
 
     useEffect(()=>{
         setComments(props.comments);
     },[props.comments]);
 
+    //delete functions
+    function deleteComment({id}) {
+        firestore.collection('comments').doc(id).delete()
+            .then(()=>{
+                const newList = comments.filter(event => event.id !== id);
+                setComments(newList);
+            })
+            .then(function () {
+                handleDeleteClose();
+            })
+            .catch(error => {console.log(error)});
+        console.log("event removed");
 
-    const headCells = [
-        {id: 'subject', label: 'Subject'},
-        {id: 'message', label: 'Message'}
-    ];
+    }
+
+    const handleDeleteOpen = (props) => {
+        setFormData({...props});
+        setDeleteOpen(true);
+    };
+
+    const handleDeleteClose = () => {
+        setDeleteOpen(false);
+    };
 
     return(
         <Container component="main" maxWidth="md" style={{textAlign: 'center'}}>
@@ -22,6 +52,14 @@ const AdminContact = (props) => {
             <EnhancedTable
                 data={comments}
                 headCells={headCells}
+                delete={handleDeleteOpen}
+            />
+            <TransitionModal
+                open={deleteOpen}
+                handleOpen={handleDeleteOpen}
+                handleClose={handleDeleteClose}
+                form={<DeleteComment formData={formData} submit={deleteComment} cancel={handleDeleteClose} />}
+                title={"Are You Sure?"}
             />
         </Container>
     )
