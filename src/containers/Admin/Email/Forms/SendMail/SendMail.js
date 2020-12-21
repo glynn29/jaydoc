85ebde76976d913607c3395e1f80da875174cf8c
glynn29 <glynn_29@hotmail.com>
2020-12-21 17:59:36 -0600

    changed list to a table for longer lists of names

--- src/containers/Admin/Email/Forms/SendMail/SendMail.js
+++ src/containers/Admin/Email/Forms/SendMail/SendMail.js
@@ -5,29 +5,44 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 
 import useStyles from "../../../../../components/UI/Styles/formStyle";
+import EnhancedTable from "../../../../../components/UI/Table/Table";
 
+
+const headCells = [
+    { id: 'email',  label: 'Email' },
+];
 
 const SendMail = props => {
     const classes = useStyles();
     let {formData} = props;
 
+    const tableData = [];
+    formData.forEach(row => {
+        tableData.push({"email": row});
+    });
     return(
         <Container component="main" maxWidth="sm" style={{textAlign: 'center'}}>
             <Grid container spacing={2}  >
-                <Grid item xs={12}>
-                    {<ul>
-                        {formData.map(row =>
-                            <li style={{textAlign: 'left'}} key={row}>
-                                {row}
-                            </li>)
-                        }
-                    </ul>}
+                <EnhancedTable
+                    data={tableData}
+                    headCells={headCells}
+                />
+                <Grid item xs={6} sm={6}>
+                    <Button
+                        variant="outlined"
+                        className={classes.cancelButton}
+                        onClick={props.cancel}
+                        fullWidth
+                    >Cancel</Button>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Button variant="outlined" className={classes.cancelButton} onClick={props.cancel}>Cancel</Button>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Button variant="contained"  className={classes.detailsButton} onClick={() => props.submit(formData)}>{props.button}</Button>
+                <Grid item xs={6} sm={6}>
+                    <Button variant="contained"
+                            color="primary"
+                            onClick={() => props.submit(formData)}
+                            fullWidth
+                    >
+                        Send Mail
+                    </Button>
                 </Grid>
             </Grid>
         </Container>
