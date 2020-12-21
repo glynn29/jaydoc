ef023d88fe9a1f6374f2c6d044758583fe8442b6
glynn29 <glynn_29@hotmail.com>
2020-12-21 16:13:13 -0600

    reformatted dates

--- src/containers/Calendar/Confirmation/Confirmation.js
+++ src/containers/Calendar/Confirmation/Confirmation.js
@@ -8,15 +8,17 @@ const Confirmation = props => {
     if(!props.formData){
         return <Redirect to={"/"}/>
     }
-
     const {formData} = props;
-    console.log(props.formData);
+
+    const formattedStart = new Date(formData.date + "T" + formData.startTime).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
+    const formattedEnd = new Date(formData.date + "T" + formData.endTime).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
+    const formattedDate = new Date(formData.date + "T17:00").toDateString();
 
     return(
         <Container component="main" maxWidth="md" style={{textAlign: 'center'}}>
             <p>Sign Up Successful!</p>
             <p>{formData.eventName}</p>
-            <p>You signed up as {formData.position} from {formData.startTime} to {formData.endTime} on {formData.date}?</p>
+            <p>You signed up as {formData.position} from {formattedStart} to {formattedEnd} on {formattedDate}?</p>
             <p>{formData.details}</p>
         </Container>
     )
