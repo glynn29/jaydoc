6c2e18cb6c80c7b008c0bb01fc9066deba14a88a
glynn29 <glynn_29@hotmail.com>
2020-12-21 18:23:12 -0600

    added formatted time

--- src/containers/Admin/ScheduledEventList/Forms/DetailsEvent/DetailsEvent.js
+++ src/containers/Admin/ScheduledEventList/Forms/DetailsEvent/DetailsEvent.js
@@ -15,9 +15,16 @@ const DetailsEvent = props => {
     const classes = useStyles();
     const formData = props.formData;
 
+    const formattedStart = new Date(formData.date + "T" + formData.start).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
+    const formattedEnd = new Date(formData.date + "T" + formData.end).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
+    const formattedDate = new Date(formData.date + "T17:00").toDateString();
+
     return(
         <Container component="main" maxWidth="sm" style={{textAlign: 'center'}}>
             <Grid container spacing={1}>
+                <Grid item xs={12}>
+                    <p>Event from {formattedStart} to {formattedEnd} on {formattedDate}</p>
+                </Grid>
                 <EnhancedTable
                     data={formData.positions}
                     headCells={headCells}
