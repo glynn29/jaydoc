35f8e8d598e5cff2d50691e60bac521946ad5a94
glynn29 <glynn_29@hotmail.com>
2021-02-18 16:00:05 -0600

    fixed item spacing on tiles

--- src/containers/Calendar/Calendar.js
+++ src/containers/Calendar/Calendar.js
@@ -1,15 +1,18 @@
 import React, {useContext, useEffect, useState} from "react";
 import 'react-calendar/dist/Calendar.css'
-
 import Calendar from 'react-calendar';
+
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+
 import * as classes from './Calendar.module.css';
 import {firestore} from "../../firebase";
 import SignUp from "./Forms/SignUp/SignUp";
 import TransitionModal from "../../components/UI/Modal/Modal";
-import Typography from "@material-ui/core/Typography";
 import {AuthContext} from "../Auth/Auth";
-import AdminSingUp from "./Forms/AdminSignUp/AdminSignUp";
-import Container from "@material-ui/core/Container";
+import AdminSignUp from "./Forms/AdminSignUp/AdminSignUp";
+import Spinner from "../../components/UI/Spinner/Spinner";
+
 
 const CalendarBox = () => {
     const {isAdmin} = useContext(AuthContext);
@@ -17,23 +20,44 @@ const CalendarBox = () => {
     const [date, setDate] = useState(new Date());
     const [formData, setFormData] = useState({});
     const [tableData, setTableData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    async function getEvents() {
+
+    async function getEvents(start, end) {
         let events = [];
-        const eventsRef = await firestore.collection('scheduledEvents').orderBy("date","asc").get();
-        eventsRef.forEach((event) => {
+
+        setLoading(true);
+        //get events only for current month
+        const eventsRef2 = await firestore.collection('scheduledEvents').where("date" , ">=", start).where("date", "<=", end).orderBy("date","asc").get();
+
+        eventsRef2.forEach((event) => {
             events.push({...event.data(), id: event.id});
         });
         setTableData(events);
+        setLoading(false);
+    }
+
+    function getDateRanges(newDate) {
+        console.log("called",newDate);
+        const year = newDate.getFullYear();
+        const month = newDate.getMonth() + 1;
+        let formattedMonth = month;
+        if (month < 10){
+            formattedMonth = "0" + month;
+        }
+        const start = year  + "-" + formattedMonth  + "-01";
+        const end = year + "-" + formattedMonth + "-31";
+        getEvents(start, end).catch(error => {console.log(error)});
     }
 
     useEffect(() => {
-        getEvents().catch(error => {console.log(error)});
+        getDateRanges(date);
     }, []);
 
     function editModal() {
+        //reload events once user has signed up
         handleModalClose();
-        getEvents().catch(error => {console.log(error)});
+        getDateRanges(date);
     }
 
     const handleModalClose = () => {
@@ -41,8 +65,7 @@ const CalendarBox = () => {
     };
 
     const handleModalOpen = (props) => {
-        //const rowDate = new Date(props.date + "T17:00");
-        //setFormData({...props, date: rowDate});
+        console.log(props);
         setFormData({...props});
         setModalOpen(true);
     };
@@ -51,6 +74,7 @@ const CalendarBox = () => {
         <Container component="main" maxWidth="lg" style={{textAlign: 'center'}}>
             <Typography variant="h3">Calendar Page</Typography>
                 <Calendar
+                    onActiveStartDateChange={({activeStartDate}) => getDateRanges(activeStartDate)}
                     className={classes.Box}
                     onChange={e => setDate(e)}
                     value={date}
@@ -65,7 +89,6 @@ const CalendarBox = () => {
                     tileContent={
                         ({ date, view }) => {
                             const table = tableData.map((row)=>{
-
                                 const currentDate = new Date();
                                 const rowDate = new Date(row.date + "T19:00");
                                 const day = rowDate.getDate();
@@ -73,18 +96,19 @@ const CalendarBox = () => {
                                 const year = rowDate.getFullYear();
                                 const isPastDate = currentDate > rowDate;
                                 const itemClass = isPastDate ? classes.PastCalenderItem : classes.CalenderItem;
+
                                 return(
                                     view === 'month' &&
                                     date.getDate() === day &&
                                     date.getMonth() === month &&
                                     date.getFullYear() === year
                                         ?
-                                        <p
-                                            key={row.id}
-                                            onClick={()=> !isPastDate && handleModalOpen(row)}
-                                            className={itemClass}>
-                                            {row.name}
-                                        </p>
+                                    <p key={row.id}
+                                       onClick={()=> handleModalOpen({...row, isPastDate})}
+                                       className={itemClass}
+                                    >
+                                        {row.name}
+                                    </p>
                                         : null
                                 );
                             });
@@ -94,13 +118,13 @@ const CalendarBox = () => {
                         }
                     }
                 />
-            <div>{date.toDateString()}</div>
+            <div>{loading && <Spinner/>}</div>
 
             <TransitionModal
                 open={modalOpen}
                 handleOpen={handleModalOpen}
                 handleClose={handleModalClose}
-                form={isAdmin ? <AdminSingUp formData={formData} cancel={handleModalClose} submit={editModal}/>:<SignUp formData={formData} onEdit={editModal} cancel={handleModalClose} getEvents={getEvents} />}
+                form={isAdmin ? <AdminSignUp formData={formData} cancel={handleModalClose} submit={editModal}/> : <SignUp formData={formData} onEdit={editModal} cancel={handleModalClose} getEvents={getDateRanges} date={date}/>}
                 title={isAdmin ? "Edit Positions" : "Sign Up"}
             />
         </Container>
