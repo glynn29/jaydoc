49e755cec917908ee51cfce37674ba6ef3aa70f7
glynn29 <glynn_29@hotmail.com>
2021-02-23 23:01:52 -0600

    added check to see if database was up to date before performing actions

--- src/containers/Calendar/Forms/AdminSignUp/AdminSignUp.js
+++ src/containers/Calendar/Forms/AdminSignUp/AdminSignUp.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import Positions from "../../../Admin/ScheduledEventList/Forms/Volunteers/Volunteers";
+import Volunteers from "../../../Admin/ScheduledEventList/Forms/Volunteers/Volunteers";
 import Container from "@material-ui/core/Container";
 import {firestore} from "../../../../firebase";
 
@@ -13,54 +13,67 @@ const headCells = [
 const AdminSingUp = (props) => {
     console.table(props.formData);
     const {start, end, date, name, eventId} = props.formData;
-    const [positions, setPositions] = useState(props.formData.positions);
-    const [signedUpUsers, setSignedUpUsers] = useState([]);
 
-    const updatePositions = (positions, signedUpUsers) => {
-        signedUpUsers.filter(row => row !== undefined);
-        console.log(signedUpUsers);
-        setSignedUpUsers(signedUpUsers);
-        setPositions(positions);
-        //handleModalClose();
-    };
-
-    const submitFormHandler = (positions, signedUpUsers) =>{
-        signedUpUsers.filter(row => row !== undefined);
-        firestore.collectionGroup("volunteerEvents").where("scheduledEventId", "==", props.formData.id).get()
-            .then(function (result) {
-                result.forEach(row => {
-                    row.ref.delete().catch(error => console.log(error));
-                });
-            }).then(function () {
-            firestore.collection('scheduledEvents').doc(props.formData.id).set({
-                positions: positions
-            },{merge: true}).catch(error => console.log(error));
-        }).then(function () {
-            signedUpUsers.forEach(user => {
-                if (user !== undefined){
-                    firestore.collection('users').doc(user.userDocId).collection('volunteerEvents').add({
-                        date,
-                        endTime: end,
-                        eventId,
-                        eventName: name,
-                        id: user.id,
-                        name: user.first + " " + user.last,
-                        position: user.position,
-                        role: user.role,
-                        startTime: start,
-                        scheduledEventId: props.formData.id
-                    }).catch(error => console.log(error));
+    const submitFormHandler = (positions) =>{
+        console.log(props.formData);
+        firestore.collection('scheduledEvents').doc(props.formData.id).get().then(function (res) {
+            console.table(res.data().positions);
+            console.table(props.formData.positions);
+            let upToDate = true;
+            for (let i = 0; i < props.formData.positions.length; i++){
+                if (props.formData.positions[i].volunteer !== res.data().positions[i].volunteer){
+                    upToDate = false;
+                    console.log(false);
                 }
-            });
-        })
-            .then(()=>{props.submit();})
-            .catch(error => {console.log(error)});
-        console.log("event Edited");
+            }
+
+            if(upToDate){
+                firestore.collectionGroup("volunteerEvents").where("scheduledEventId", "==", props.formData.id).get()
+                    .then(function (result) {
+                        result.forEach(row => {
+                            row.ref.delete().catch(error => console.log(error));
+                        });
+                    }).then(function () {
+                    firestore.collection('scheduledEvents').doc(props.formData.id).set({
+                        positions: positions
+                    },{merge: true}).catch(error => console.log(error));
+                }).then(function () {
+                    positions.forEach(user => {
+                        if (user.email){
+                            firestore.collection("users").where("email", "==", user.email).get().then(function (result) {
+                                result.forEach((row) =>{
+                                    console.log(row.data(), row.id);
+                                    firestore.collection('users').doc(row.id).collection('volunteerEvents').add({
+                                        date,
+                                        endTime: end,
+                                        eventId,
+                                        eventName: name,
+                                        id: row.data().id,
+                                        name: row.data().first + " " + row.data().last,
+                                        position: user.position,
+                                        role: row.data().role,
+                                        startTime: start,
+                                        scheduledEventId: props.formData.id
+                                    }).catch(error => console.log(error));
+                                })
+                            });
+                        }
+                    });
+                })
+                    .then(()=>{props.submit();})
+                    .catch(error => {console.log(error)});
+                console.log("event Edited");
+            }else {
+                props.submit();
+                alert("Error, someone may have just signed up, please try again");
+            }
+        });
+
     };
 
     return(
-        <Container component="main" maxWidth="lg" style={{textAlign: 'center'}}>
-            <Positions cancel={props.cancel} submit={submitFormHandler} positions={positions} headCells={headCells} signedUpUsers={signedUpUsers} isPastDate={props.formData.isPastDate}/>
+        <Container component="main" maxWidth="md" style={{textAlign: 'center'}}>
+            <Volunteers cancel={props.cancel} submit={submitFormHandler} positions={props.formData.positions} headCells={headCells} isPastDate={props.formData.isPastDate}/>
         </Container>
     );
 };
