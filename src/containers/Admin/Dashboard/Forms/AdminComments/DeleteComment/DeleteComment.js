fc94d6227c7a816b3e69f23a4f987bfca1d3bd54
glynn29 <glynn_29@hotmail.com>
2021-03-11 16:31:53 -0600

    moved folder

--- src/containers/Admin/Dashboard/Forms/AdminComments/DeleteComment/DeleteComment.js
+++ src/containers/Admin/Dashboard/Forms/AdminComments/DeleteComment/DeleteComment.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
-import useStyles from "../../../../../components/UI/Styles/formStyle";
+import useStyles from "../../../../../../components/UI/Styles/formStyle";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
